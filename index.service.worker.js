26231003d0bc2698e9c944fa5db0a0c550a6daf5
tobi-awolaju (kawaii) <120692959+kawaiix621@users.noreply.github.com>
2025-01-05 01:13:11 +0100

    :_)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,14 +4,14 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736034432|125560145';
+const CACHE_VERSION = '1736035983|1676425063';
 /** @type {string} */
 const CACHE_PREFIX = 'BaseBros-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
-const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
+const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 // Files that will be cached on load.
 /** @type {string[]} */
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.worker.js","index.audio.worklet.js"];
